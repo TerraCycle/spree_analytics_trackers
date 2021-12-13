065e74921e95a13aafa6ac0603517992b432e827
Erik Csonka <erik.csonka@terracycle.com>
2021-12-13 15:01:16 +0100

    Add locale param to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Spree::Core::Engine.add_routes do
-  namespace :admin, path: Spree.admin_path do
-    resources :trackers
+  scope '(:locale)', locale: /#{Spree.available_locales.join('|')}/, defaults: { locale: nil } do
+    namespace :admin, path: Spree.admin_path do
+      resources :trackers
+    end
   end
 end
